ebbb942d4ee92d03582229e6e777eb07bdfa9e2d
Vlad Listgarten <listgarten100@gmail.com>
2021-01-21 23:31:29 +0200

    feat: create the function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 //start task
 module.exports = function reverse (n) {
-  
-}
+  if(n < 0){
+    n*=(-1);
+  }
+  let numb = '';
+n.toString().split('').reverse().forEach(elem =>{
+numb += elem;
+});
+return +numb;
+};
