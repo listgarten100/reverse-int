766a57c5242cde34ded63099412200f5f1ded188
Vlad Listgarten <listgarten100@gmail.com>
2021-01-21 23:34:56 +0200

    refactor: add formatting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 //start task
 module.exports = function reverse (n) {
-  if(n < 0){
-    n*=(-1);
-  }
   let numb = '';
-n.toString().split('').reverse().forEach(elem =>{
-numb += elem;
-});
-return +numb;
+
+  if(n < 0) n*=(-1); 
+
+  let arr = n.toString().split('').reverse();
+  arr.forEach(elem =>{
+    numb += elem;
+  });
+  
+  return +numb;
 };
